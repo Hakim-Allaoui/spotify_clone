57765738176e175de54be70d4deb697f5f1c0b15
Hakim Allaoui <hakim.Allaoui97@gmail.com>
2021-10-12 11:56:17 +0100

    improvements

---
+++ lib/utils/ads_helper.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NewPage extends StatefulWidget {
+  @override
+  _NewPageState createState() => _NewPageState();
+}
+
+class _NewPageState extends State<NewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
